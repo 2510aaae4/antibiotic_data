2a7881b66d4531fcd7250ec19dc3a7d22438739e
2510aaae4 <45693357+2510aaae4@users.noreply.github.com>
2023-04-26 12:29:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@
 
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import pandas as pd
